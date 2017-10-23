0730d2e69fdd11f26868015195bc1d4103ea0281
janteloo <janteloo@gmail.com>
2017-10-23 07:31:51 -0300

    Update MailSender.php

--- php/MailSender.php
+++ php/MailSender.php
@@ -13,7 +13,7 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'janteloo@gmail.com';                 // SMTP username
+    $mail->Username = '***';                 // SMTP username
     $mail->Password = '***';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
